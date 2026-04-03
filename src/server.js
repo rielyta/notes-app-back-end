ca413bccf0d7eabcb4c4ad6ebf16328c91e74a56
rielyta <desi@students.usu.ac.id>
2026-04-03 19:45:17 +0700

    -'

--- src/server.js
+++ src/server.js
@@ -1,21 +1,15 @@
-import https from 'https';
-import fs from 'fs';
 import express from 'express';
 import routes from './routes.js';
 import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
 
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use('/', routes);
 
-const options = {
-  key: fs.readFileSync('./ssl/key.pem'),
-  cert: fs.readFileSync('./ssl/cert.pem')
-};
-
-https.createServer(options, app).listen(port, () => {
-  console.log(`Server running at https://0.0.0.0:${port}`);
+app.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}`);
 });
