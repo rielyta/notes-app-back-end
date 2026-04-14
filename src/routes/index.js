5dac0227a846a850ea67359749cad8a239b9c0fd
rielyta <desi@students.usu.ac.id>
2026-04-14 17:15:34 +0700

    -

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import notes from '../services/notes/routes/index.js';
-
+import users from '../services/users/routes/index.js';
 
 const router = Router();
 
 router.use('/', notes);
+router.use('/', users);
 
 export default router;
